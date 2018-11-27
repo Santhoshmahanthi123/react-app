57ce7b3b0f62b2ebe310553cd3927b790dd3951d
santhosh <singumahanthisanthosh@gmail.com>
2018-11-27 19:14:28 +0530

    rendering props individually dynamically

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import First from './First'
 class App extends Component {
+  //map function takes an array in java script and maps it to new array
+  state = {
+    first : [
+    {name : 'Santhosh', age : '22', id:1},
+    {name : 'Pandu', age :'23', id :2},
+    {name : 'Bharath', age :'24', id :3 }
+  ]
+  }
   render() {
     return (
       <div className="App">
           <h1>Welcome to React Application.</h1>
           <p>A simple react application react application</p>
-          <First name="Santhosh" age="22"/>
+          <First first={this.state.first}/>
       </div>
     );
   }
--- src/First.js
+++ src/First.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 class First extends Component{
+ 
     render(){
         //child component to be rendered in to parent component App.js
        //console.log(this.props)
        //destructuring to grab all properties
-       const {name, age} = this.props;
-       return(
-            <div className="First">
-                <div>Name : {name}</div>
-                <div>Age : {age}</div>
-            </div>
+       const {first} = this.props;
+       const firstList = first.map(first =>{
+           //key is used to identify which element is to render in to dom according to id
+          return (
+                <div className="First" key={first.id}>
+                <div>Name : {first.name}</div>
+                <div>Age : {first.age}</div>
+                </div>
         )
+       })
+       return(
+           <div className="first-list">
+             {firstList}  
+           </div>
+       )
+            
     }
 }
 
