cb69e23f8ac79646eccd22e34a8ea301733fc92c
santhosh <singumahanthisanthosh@gmail.com>
2018-11-27 19:34:21 +0530

    destructuring removed added directly to function in place of props

--- src/First.js
+++ src/First.js
@@ -1,12 +1,12 @@
 import React from 'react';
 //UI based components uses functions which get data from the props
 //UI based components dont require render method like class based
-const First = (props) =>{
+const First = ({first}) =>{
        //child component to be rendered in to parent component App.js
        //destructuring to grab all properties
        //in UI based components this.props not used since
        //it aquires all props as a function but not as a class
-       const {first} = props;
+    //   This recieves props so directly put this in function as above const {first} = props;
        const firstList = first.map(first =>{
            //key is used to identify which element is to render in to dom according to id
           return (
